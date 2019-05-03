b3e73d4ebfe1a361fbb4e52d0588cfc0f61224fb
Rodolfo Llinás <rllinasc@unal.edu.co>
2019-05-03 05:27:39 -0500

    added feature bubble_sort_by

--- bubble_sort_by.rb
+++ bubble_sort_by.rb
@@ -3,14 +3,18 @@ def bubble_sort_by(array)
     loop do 
         swapped = false
         (n - 1).times do |i|
-            if yield(array[i], array[i + 1]) == 1
+            if block_given?
+                if yield(array[i], array[i + 1]) == 1
                 array[i], array[i+1] = array[i+1], array[i]
                 swapped = true
+            else
+                if(array[i], array[i + 1]) == 1
+                    array[i], array[i+1] = array[i+1], array[i]
+                    swapped = true
+                end
             end
         end
         break if swapped == false
     end
-    print array
-    puts
 end
 bubble_sort_by(["hi","hello","hey"]) {|left, rigth| left.length <=> rigth.length}
